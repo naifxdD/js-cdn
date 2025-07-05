20a8e855d87d9d293855bd4f4aeb44fbfc40c6da
naifxdD <naifaldair@gmail.com>
2025-07-05 14:34:16 +0300

    s

---
+++ script.js
@@ -0,0 +1,21 @@
+(function () {
+    var cookieName = 'ab_test_group';
+    // check existing group
+    function getCookie(name) {
+        var v = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
+        return v ? v.pop() : '';
+    }
+    var group = getCookie(cookieName);
+    if (!group) {
+        // 50/50 split
+        group = (Math.random() < 0.5) ? 'A' : 'B';
+        document.cookie = cookieName + '=' + group + '; path=/';
+    }
+    // act on bucket
+    if (group === 'B') {
+        // window.location.href = '/variant-B.html'
+        console.log('User in group B');
+    } else {
+        console.log('User in group A');
+    }
+})();
